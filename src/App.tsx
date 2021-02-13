66efafc1eefe2528244e83d927baa215c3e8a520
charlie wilhite <ctbwilhite@gmail.com>
2021-02-13 13:08:41 -0600

    reconfigured typescript config

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { Text } from 'rebass/styled-components';
-
-const App = () => <Text>Under Construction</Text>
-
-export default App;
--- src/theme/index.ts
+++
@@ -1,7 +0,0 @@
-export const theme = {
-  name: 'ctbw.dev',
-  fonts: {
-    body: 'Noto Serif, Helvetica, Times New Roman, sans-serif',
-    heading: 'Noto Serif, Helvetica, Times New Roman, sans-serif',
-  },
-};
