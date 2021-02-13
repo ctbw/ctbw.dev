ec42f269bc8bc6c3c4ec21aaae6409a1c0175025
Charlie Wilhite <ctbwilhite@gmail.com>
2021-02-13 11:07:21 -0600

    converted to TS, added some global context config

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Text } from 'rebass/styled-components';
+
+const App = () => <Text>Under Construction</Text>
+
+export default App;
---
+++ src/theme/index.ts
@@ -0,0 +1,7 @@
+export const theme = {
+  name: 'ctbw.dev',
+  fonts: {
+    body: 'Noto Serif, Helvetica, Times New Roman, sans-serif',
+    heading: 'Noto Serif, Helvetica, Times New Roman, sans-serif',
+  },
+};
