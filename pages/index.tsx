6350645e96fd9d31d53140a9ac2a1e4b5919516b
charlie <ctbwilhite@gmail.com>
2021-02-20 14:25:51 -0600

    Configured notion-api-worker

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { NotionRenderer, BlockMapType } from "react-notion";
+// import Head from "next/head";
+// import Link from "next/link";
+import fetch from "node-fetch";
 
-const Index = () => {
-    return <div>Under Construction</div>;
+export const getStaticProps = async () => {
+  const data: BlockMapType = await fetch(
+    "https://notion-api.splitbee.io/v1/page/459d756a55764fc0968bc30331086f4a"
+  ).then((res: any) => res.json());
+
+  return {
+    props: {
+      blockMap: data
+    },
+    revalidate: 1
+  };
 }
+
+export interface IndexPropsType {
+    blockMap: BlockMapType;
+}
+
+const Index = ({ blockMap }: IndexPropsType) => {
+    console.log('Block Map:', blockMap);
+
+    return (<div>under construction</div>)
+};
 
 export default Index;
