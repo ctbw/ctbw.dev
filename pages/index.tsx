1cd55c345c5807d02d8f8284a7d0f595bffbd0c4
charlie <ctbwilhite@gmail.com>
2021-02-22 10:19:39 -0600

    Trying a different auth approach

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,13 @@ import { NotionRenderer, BlockMapType } from 'react-notion';
 import fetch from 'node-fetch';
 
 export const getStaticProps = async () => {
-  const main = 'https://notion-api.splitbee.io/v1/page/459d756a55764fc0968bc30331086f4a';
-  const data: BlockMapType = await fetch(main).then((res: any) => res.json());
+  const main = 'https://notion.ctbw.workers.dev/v1/page/08804191115447ce8532a9d67a5ee397';
+  const data = await fetch(main, {
+    method: 'get',
+    headers: {
+      Authorization: `Bearer 44d8782f3fcafa2f206a09b525535711f4ed3b50afbe6a4deb358f6333c8ef8e90a8bc485e0b94d93722e397a91add0163b3244bfff648579cad7da0e7e5fd604ade837de613ad4c1ee72f95d41b`,
+    },
+  }).then((res: any) => res.json());
 
   return {
     props: {
@@ -22,7 +27,13 @@ export interface IndexPropsType {
 const Index = ({ blockMap }: IndexPropsType) => {
   console.log('Block Map:', blockMap);
 
-  return <div>under construction</div>;
+  // return (
+  //   <>
+  //     <NotionRenderer blockMap={blockMap} />
+  //   </>
+  // );
+
+  return <>hello world</>;
 };
 
 export default Index;
